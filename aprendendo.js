865503bf385587c28be977e02bbae6775ba5aef4
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-09-28 16:02:22 -0300

    Implementando models e inserindo dados

--- aprendendo.js
+++ aprendendo.js
@@ -9,3 +9,45 @@ mongoose.connect("mongodb://localhost/aprendendo", {
 }).catch((err) => {
     console.log("Houve um erro ao se conectar ao mongoDB: " + err)
 })
+
+//MODEL - USUÁRIOS
+    //definindo o model    
+    const UsuarioSchema = mongoose.Schema({
+
+        nome: {
+            type: String,
+            require: true //requerido, preenchimento obrigatório   
+        },
+        sobrenome: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        idade: {
+            type: Number,
+            require: true
+        },
+        pais: {
+            type: String
+        }    
+    })
+
+// Collection
+    mongoose.model('usuarios', UsuarioSchema)
+
+const Aline = mongoose.model('usuarios')
+
+    new Aline({
+        nome: "Aline",
+        sobrenome: "Barbosa",
+        email: "aline@gmail.com",
+        idade: 21,
+        pais: "Brasil"
+    }).save().then(() => {
+        console.log("Usuário criado com sucesso!")
+    }).catch((err) => {
+        console.log("Houve um erro ao registrar o usuário" + err)
+    })
